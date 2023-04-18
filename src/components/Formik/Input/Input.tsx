d5aea67fe78370b41a3b4a7b2ea60c11033d5ae9
Alendart <111238215+Alendart@users.noreply.github.com>
2023-04-18 23:56:16 +0200

    Prepared reusable component for Formik Input

---
+++ src/components/Formik/Input/Input.tsx
@@ -0,0 +1,30 @@
+import { useField } from "formik";
+import React from "react";
+
+import "./Input.css"
+
+interface Props {
+    label:string;
+    name:string;
+    type:string;
+    placeholder:string;
+}
+
+export const Input = (props: Props) => {
+    const [field, meta] = useField({
+        name: props.name,
+        type: props.type,
+    });
+
+    return(
+        <div className="input-box">
+            <label htmlFor={props.name}>{props.label}</label>
+            <input className={props.name} id={props.name} {...field} {...props}/>
+            {
+                meta.touched && meta.error ?
+                    <div className="error">{meta.error}</div>
+                    : null
+            }
+        </div>
+    )
+}
