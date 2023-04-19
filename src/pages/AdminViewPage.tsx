6434bbfba59cea49c2457dee6584fcbeb5d483bb
JarekKrolik <madmax@onet.eu>
2023-04-19 19:30:50 +0200

    added admin view header and css styles

--- src/pages/AdminViewPage.tsx
+++ src/pages/AdminViewPage.tsx
@@ -1,10 +1,19 @@
 import React from "react";
+import '../../src/scss/AdmivViewPage.scss'
 
 export const AdminViewPage = ()=>{
 
 
 
     return(
-        <div>widok admina</div>
+        <div className={'admin-view'}>
+<header className={'admin-view__header'}>
+    <div className="admin-view__header-picture">
+        <img src={require('../assets/images/MegaK.png')} alt=""/>
+    </div>
+    <h2>panel admina</h2>
+    <button>ustawienia</button>
+</header>
+        </div>
     )
 }
