f9e758e1a4708b257c232b7d37bebf9ddabbb1ce
JarekKrolik <madmax@onet.eu>
2023-04-21 17:46:34 +0200

    removed unused import from index.tsx, fixed css styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './utils/scss/index.scss';
-import './index.scss';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter}from'react-router-dom';
 
