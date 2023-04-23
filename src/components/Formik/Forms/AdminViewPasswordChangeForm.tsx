4dc89bd702e002c746bd0e530c60c32b6d4ecdc2
JarekKrolik <madmax@onet.eu>
2023-04-23 10:03:20 +0200

    added passwordCheck to initial values and interface

--- src/components/Formik/Forms/AdminViewPasswordChangeForm.tsx
+++ src/components/Formik/Forms/AdminViewPasswordChangeForm.tsx
@@ -1,36 +1,43 @@
 import React from 'react';
-import {Formik, Form,  FormikHelpers} from 'formik';
+import {Formik, Form, FormikHelpers} from 'formik';
 import {Input} from "../Input/Input";
-interface Props{
-    handleModalExit:()=>void;
-}
-interface Password{
-    password:string;
+
+interface Props {
+    handleModalExit: () => void;
 }
 
-export const AdminViewPasswordChangeForm = (props:Props)=>{
+interface Password {
+    password: string;
+    passwordCheck: string;
+}
+
+export const AdminViewPasswordChangeForm = (props: Props) => {
 
 
+    return (
 
-    return(
-
-<Formik initialValues={{password:''}}    onSubmit={(
-    values: Password,
-    { setSubmitting }: FormikHelpers<Password>
-) => {
-    setTimeout(() => {
-        alert(JSON.stringify(values, null, 2));
-        setSubmitting(false);
-    }, 500);
-}}>
-    <Form className={'admin-view__form'}>
-        <p>nowe hasło</p>
-    <Input label={''} name={'password'} type={'password'} placeholder={'hasło'}/>
-        <p>powtórz hasło</p>
-    <Input label={''} name={'passwordCheck'} type={'confirm'} placeholder={'powtórz hasło'}/>
-        <button className={'btn'} type={'submit'}>wyślij</button>
-        <button onClick={props.handleModalExit} className={'btn modal'}>zamknij</button>
-    </Form>
-</Formik>
+        <Formik initialValues={
+            {
+                password: '',
+                passwordCheck: '',
+            }
+        } onSubmit={(
+            values: Password,
+            {setSubmitting}: FormikHelpers<Password>
+        ) => {
+            setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+            }, 500);
+        }}>
+            <Form className={'admin-view__form'}>
+                <p>nowe hasło</p>
+                <Input label={''} name={'password'} type={'password'} placeholder={'hasło'}/>
+                <p>powtórz hasło</p>
+                <Input label={''} name={'passwordCheck'} type={'confirm'} placeholder={'powtórz hasło'}/>
+                <button className={'btn'} type={'submit'}>wyślij</button>
+                <button onClick={props.handleModalExit} className={'btn modal'}>zamknij</button>
+            </Form>
+        </Formik>
     )
 }
