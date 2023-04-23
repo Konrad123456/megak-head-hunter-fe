e342cf3a9ed770f99404670b79c7605c818b837c
JarekKrolik <madmax@onet.eu>
2023-04-23 13:30:09 +0200

    adding file validation in add students form

--- src/components/Formik/Forms/AdminViewFormStudentsAdd.tsx
+++ src/components/Formik/Forms/AdminViewFormStudentsAdd.tsx
@@ -1,41 +1,50 @@
 import React from "react";
-import {Form, Formik, FormikHelpers} from "formik";
+import {Form, Formik, FormikHelpers, useField} from "formik";
 import {Input} from "../Input/Input";
 import {Button} from "../../Button/Button";
+import * as Yup from "yup";
+
 interface Values {
     studentsFile: null;
 
 }
+
 interface Props {
-    handleModalExit:()=>void;
+    handleModalExit: () => void;
 }
 
-export const AdminViewFormStudentsAdd = (props:Props)=>{
 
+
+export const AdminViewFormStudentsAdd = (props: Props) => {
 
     return (
         <Formik
             initialValues={{
-               studentsFile:null,
+                studentsFile: null,
             }}
-
+            validationSchema={Yup.object({
+                studentsFile:Yup.mixed().required('Musisz dodać plik')
+            })
+           }
 
 
             onSubmit={(
                 values: Values,
-                { setSubmitting }: FormikHelpers<Values>
+                {setSubmitting}: FormikHelpers<Values>
             ) => {
                 setTimeout(() => {
+                    console.log(typeof values.studentsFile)
                     alert(JSON.stringify(values, null, 2));
                     setSubmitting(false);
                 }, 500);
             }}
         >
             <Form className={'admin-view__form'}>
-                <Input label={'Importuj listę kursantów'} name={'studentsFile'} type={'file'} placeholder={''}/>
-                <Button endpoint={'#'} text={'wyślij'}/>
+                {/*<Input label={'Importuj listę kursantów'} name={'studentsFile'} type={'file'} placeholder={''}/>*/}
+                {/*<Button endpoint={'#'} text={'wyślij'}/>*/}
+                <input onChange={(event)=>event.currentTarget.files?console.log(event.currentTarget.files[0]):null} type="file" name={'studentsFile'}/>
+                <button type={'submit'} className="btn">wyslij</button>
                 <button onClick={props.handleModalExit} className={'btn modal'}>zamknij</button>
             </Form>
         </Formik>
-    )
-}
+    )}
