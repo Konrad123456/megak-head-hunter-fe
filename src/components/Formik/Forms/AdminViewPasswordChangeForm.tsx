f4b2e8eebe88a8eafca80d98ffd51f284796e0b6
JarekKrolik <madmax@onet.eu>
2023-04-23 10:16:36 +0200

    fixed password check validation in password change form, added submit button re-usable component

--- src/components/Formik/Forms/AdminViewPasswordChangeForm.tsx
+++ src/components/Formik/Forms/AdminViewPasswordChangeForm.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Formik, Form, FormikHelpers} from 'formik';
 import {Input} from "../Input/Input";
+import * as Yup from "yup";
+import {Button} from "../../Button/Button";
 
 interface Props {
     handleModalExit: () => void;
@@ -20,22 +22,27 @@ export const AdminViewPasswordChangeForm = (props: Props) => {
             {
                 password: '',
                 passwordCheck: '',
-            }
-        } onSubmit={(
-            values: Password,
-            {setSubmitting}: FormikHelpers<Password>
-        ) => {
-            setTimeout(() => {
-                alert(JSON.stringify(values, null, 2));
-                setSubmitting(false);
-            }, 500);
-        }}>
+            }}
+                validationSchema={Yup.object({
+                    password: Yup.string()
+                        .required("Pole wymagane")
+                        .min(4, "Hasło musi mieć minimum 4 znaki"),
+                    passwordCheck: Yup.string().label('Powtórz hasło')
+                        .required("Pole wymagane").oneOf([Yup.ref('password')], 'Hasła muszą być takie same')
+                })}
+                onSubmit={(
+                    values: Password,
+                    {setSubmitting}: FormikHelpers<Password>
+                ) => {
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                    }, 500);
+                }}>
             <Form className={'admin-view__form'}>
-                <p>nowe hasło</p>
-                <Input label={''} name={'password'} type={'password'} placeholder={'hasło'}/>
-                <p>powtórz hasło</p>
-                <Input label={''} name={'passwordCheck'} type={'confirm'} placeholder={'powtórz hasło'}/>
-                <button className={'btn'} type={'submit'}>wyślij</button>
+                <Input label={'Nowe hasło'} name={'password'} type={'password'} placeholder={'hasło'}/>
+                <Input label={'Powtórz hasło'} name={'passwordCheck'} type={'confirm'} placeholder={'powtórz hasło'}/>
+                <Button endpoint={'#'} text={'wyślij'}/>
                 <button onClick={props.handleModalExit} className={'btn modal'}>zamknij</button>
             </Form>
         </Formik>
