fd4986f629373e784f3dc5936fa7880eb81e2ff5
JarekKrolik <madmax@onet.eu>
2023-04-26 23:11:51 +0200

    added submitBtn.tsx and SubmitBtn.scss files

---
+++ src/components/common/SubmitBtn/SubmitBtn.tsx
@@ -0,0 +1,12 @@
+import "./_SubmitBtn.scss";
+
+type Props = {
+    text: string;
+};
+export const SubmitBtn = ({ text }: Props) => {
+    return (
+        <button type="submit" className="submit-button">
+            {text}
+        </button>
+    );
+};
