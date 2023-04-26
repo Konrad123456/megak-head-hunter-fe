f6b9dc62f93c99b58a205420a18f072e92bbac1c
JarekKrolik <madmax@onet.eu>
2023-04-26 08:36:43 +0200

    added route do UserPage, path /user

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import {LoginPage} from "./pages/LoginPage/LoginPage";
 import {AdminViewPage} from "./pages/AdminViewPage/AdminViewPage";
+import {UserPage} from "./pages/userPage/UserPage";
 
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
         <Routes>
             <Route path={'/'} element={<LoginPage/>}/>
             <Route path={'/admin'} element={<AdminViewPage/>}/>
+            <Route path={'/user'} element={<UserPage/>}/>
+            <Route path={'*'} element={<LoginPage/>}/>
         </Routes>
 
     )
