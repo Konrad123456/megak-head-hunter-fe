2a0e9f61a43bdc3cbbaf8edeb42a8d4939f0eb76
JarekKrolik <madmax@onet.eu>
2023-04-26 08:30:31 +0200

    added route do AdminViewPage, path /admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import {Route,Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import {LoginPage} from "./pages/LoginPage/LoginPage";
+import {AdminViewPage} from "./pages/AdminViewPage/AdminViewPage";
 
 
 function App() {
 
-  return (
+    return (
 
-      <Routes>
-        <Route path={'/'} element={<LoginPage/>}/>
-      </Routes>
+        <Routes>
+            <Route path={'/'} element={<LoginPage/>}/>
+            <Route path={'/admin'} element={<AdminViewPage/>}/>
+        </Routes>
 
-  )
+    )
 }
 
 export default App;
