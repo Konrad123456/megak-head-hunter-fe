04e2a87e7a998c81607ca3b15dd597be8f6b5910
Alendart <111238215+Alendart@users.noreply.github.com>
2023-04-28 23:29:05 +0200

    Refactured whole UserPage into Formik Inputs, working stable. Updated styling for UserPage

--- src/components/Formik/Forms/UserViewForm.tsx
+++ src/components/Formik/Forms/UserViewForm.tsx
@@ -5,6 +5,10 @@ import {ContractType,expectedTypeWorkEntity,OneStudentResponse} from 'types'
 
 import {Input} from "../Input/Input";
 import staticText from "../../../languages/en.pl";
+import {Select} from "../Select/Select";
+import {Checkbox} from "../Checkbox/Checkbox";
+import {Textarea} from "../Textarea/Textarea";
+import {SubmitBtn} from "../../common/SubmitBtn/SubmitBtn";
 
 const expectedContractTypesValues = Object.keys(ContractType).filter(e => e.length > 1 )
 const expectedTypeWorkValues = Object.keys(expectedTypeWorkEntity).filter(e => e.length > 1 )
@@ -23,7 +27,7 @@ export interface userFullData{
     expectedTypeWork: string,
     targetWorkCity:string;
     expectedSalary: number;
-    canTakeApprenticeship: 0 | 1;
+    canTakeApprenticeship: boolean;
     monthsOfCommercialExp: number;
     education: string;
     workExperience: string;
@@ -37,12 +41,12 @@ interface Props {
 export const UserViewForm = (props:Props) => {
     const [userFEData, setUserFEData] = useState<userFullData>({
         ...props.userData,
-        portfolioUrls:props.userData.portfolioUrls.reduce((prev,curr) => prev + "\n" + curr,""),
-        projectUrls:props.userData.projectUrls.reduce((prev,curr) => prev + "\n" + curr,""),
+        portfolioUrls:props.userData.portfolioUrls.reduce((prev,curr) => prev + curr + "\n" ,""),
+        projectUrls:props.userData.projectUrls.reduce((prev,curr) => prev + curr + "\n" ,""),
         expectedContractType:expectedContractTypesValues[props.userData.expectedContractType],
         expectedTypeWork:expectedTypeWorkValues[props.userData.expectedTypeWork],
+        canTakeApprenticeship: !!props.userData.canTakeApprenticeship
     })
-
 
     return(
         <>
@@ -59,25 +63,26 @@ export const UserViewForm = (props:Props) => {
                     lastName: Yup.string()
                         .required("Pole wymagane"),
                     githubUsername: Yup.string()
-                        .required("Pole wymagane")
-                        .test(
-                            "APi check",
-                            "Taki login nie istnieje",
-                            (value) => {
-                                let check;
-                                (async () => {
-                                    const res = await fetch(`https://api.github.com/users/${value}`);
-                                    const data = await res.json();
-                                    if(data.id){
-                                        return check = true;
-                                    } else {
-                                        return check = false;
-                                    }
-                                })()
-
-                                return check
-                            }
-                        ),
+                        .required("Pole wymagane"),
+                        // Taki test jest ok ale nie tutaj, yup robi to po każdej literce, przy długim loginie ktoś zabije rate-limiter na githubie
+                        //  .test(
+                        //     "APi check",
+                        //     "Taki login nie istnieje",
+                        //     (value) => {
+                        //
+                        //         (async () => {
+                        //             const res = await fetch(`https://api.github.com/users/${value}`);
+                        //             if(res.status === 200){
+                        //                 console.log("jest ok")
+                        //                 return true;
+                        //             } else {
+                        //                 console.log("jest źle")
+                        //                 return false;
+                        //             }
+                        //         })()
+                        //
+                        //     }
+                        // ),
                     tel:Yup.string(),
                     portfolioUrls:Yup.string(),
                     projectUrls:Yup.string()
@@ -91,8 +96,7 @@ export const UserViewForm = (props:Props) => {
                         .required("Pole wymagane")
                         .oneOf(expectedContractTypesValues),
                     expectedSalary:Yup.string(),
-                    canTakeApprenticeship: Yup.number()
-                        .oneOf([0,1]),
+                    canTakeApprenticeship: Yup.boolean(),
                     monthsOfCommercialExp: Yup.number()
                         .required("Pole wymagane"),
                     education: Yup.string(),
@@ -149,80 +153,69 @@ export const UserViewForm = (props:Props) => {
                         <h2>{userPageText.columnTitle.employmentData}</h2>
                         <div className="input-container">
                             <div className="input_col1">
-                                <div className="user-page__input-box">
-                                    <label htmlFor="targetWorkCity">{userPageText.input.targetWorkCity.label}</label>
-                                    <input id="targetWorkCity" type="text" placeholder={userPageText.input.targetWorkCity.placeholder}/>
-                                </div>
-                                <div className="user-page__input-box">
-                                    <label htmlFor="expectedTypeWork">{userPageText.select.expectedTypeWork.label}</label>
-                                    <select id="expectedTypeWork" >
-                                        <option value="">{userPageText.select.expectedTypeWork.option.main}</option>
-                                        <option value="onSite">{userPageText.select.expectedTypeWork.option.onSite}</option>
-                                        <option value="move">{userPageText.select.expectedTypeWork.option.move}</option>
-                                        <option value="remote">{userPageText.select.expectedTypeWork.option.remote}</option>
-                                        <option value="partiallyRemote">{userPageText.select.expectedTypeWork.option.partiallyRemote}</option>
-                                    </select>
-                                </div>
-                                <div className="user-page__input-box">
-                                    <label htmlFor="expectedContractType">{userPageText.select.expectedContractType.label}</label>
-                                    <select id="expectedContractType" >
-                                        <option value="">{userPageText.select.expectedContractType.option.main}</option>
-                                        <option value="coe">{userPageText.select.expectedContractType.option.coe}</option>
-                                        <option value="b2b">{userPageText.select.expectedContractType.option.b2b}</option>
-                                        <option value="mandate">{userPageText.select.expectedContractType.option.mandate}</option>
-                                    </select>
-                                </div>
-                                <div className="user-page__input-box">
-                                    <label htmlFor="expectedSalary">{userPageText.input.expectedSalary.label}</label>
-                                    <input id="expectedSalary" type="number" placeholder={userPageText.input.expectedSalary.placeholder}/>
-                                </div>
-                                <div className="user-page__input-box">
-                                    <label htmlFor="monthsOfCommercialExp">{userPageText.input.monthsOfCommercialExp.label}</label>
-                                    <input id="monthsOfCommercialExp" type="number" placeholder={userPageText.input.monthsOfCommercialExp.placeholder}/>
-                                </div>
+                                <Input
+                                    classType={"user-page"}
+                                    label={userPageText.input.targetWorkCity.label}
+                                    name={"targetWorkCity"}
+                                    type={"text"}
+                                    placeholder={userPageText.input.targetWorkCity.placeholder}
+                                />
+                                <Select
+                                    classType={"user-page"}
+                                    label={userPageText.select.expectedTypeWork.label}
+                                    name={"expectedTypeWork"}
+                                >
+                                    <option value="IRRELEVANT">{userPageText.select.expectedTypeWork.option.main}</option>
+                                    <option value="ATLOCATION">{userPageText.select.expectedTypeWork.option.onSite}</option>
+                                    <option value="READY_TO_CARRYOUT">{userPageText.select.expectedTypeWork.option.move}</option>
+                                    <option value="ONLY_REMOTELY">{userPageText.select.expectedTypeWork.option.remote}</option>
+                                    <option value="HYBRID">{userPageText.select.expectedTypeWork.option.partiallyRemote}</option>
+                                </Select>
+                                <Select
+                                    classType={"user-page"}
+                                    label={userPageText.select.expectedContractType.label}
+                                    name={"expectedContractType"}
+                                >
+                                    <option value="NO_PREFERENCE">{userPageText.select.expectedContractType.option.main}</option>
+                                    <option value="UOP_ONLY">{userPageText.select.expectedContractType.option.coe}</option>
+                                    <option value="B2B_POSSIBLE">{userPageText.select.expectedContractType.option.b2b}</option>
+                                    <option value="UZ_UOD_POSSIBLE">{userPageText.select.expectedContractType.option.mandate}</option>
+                                </Select>
+                                <Input
+                                    classType={"user-page"}
+                                    label={userPageText.input.expectedSalary.label}
+                                    name={"expectedSalary"}
+                                    type={"number"}
+                                    placeholder={userPageText.input.expectedSalary.placeholder}
+                                />
+                                <Input
+                                    classType={"user-page"}
+                                    label={userPageText.input.monthsOfCommercialExp.label}
+                                    name={"monthsOfCommercialExp"}
+                                    type={"number"}
+                                    placeholder={userPageText.input.monthsOfCommercialExp.placeholder}
+                                />
                             </div>
                             <div className="input_col2">
-                                <div className="user-page__input-box checkbox">
-                                    <label htmlFor="canTakeApprenticeship">{userPageText.checkbox.canTakeApprenticeship}</label>
-                                    <input id="canTakeApprenticeship" type="checkbox" />
-                                </div>
+                                <Checkbox classType={"user-page"} name={"canTakeApprenticeship"} label={userPageText.checkbox.canTakeApprenticeship}/>
                             </div>
                         </div>
                     </div>
                     <div className="user-page__col3">
                         <h2>{userPageText.columnTitle.aboutMe}</h2>
-                        <div className="user-page__input-box">
-                            <label htmlFor="bio">{userPageText.textarea.bio.label}</label>
-                            <textarea id="bio" rows={4} placeholder={userPageText.textarea.bio.placeholder}/>
-                        </div>
-                        <div className="user-page__input-box">
-                            <label htmlFor="portfolioUrls">{userPageText.textarea.portfolioUrls.label}</label>
-                            <textarea id="portfolioUrls" rows={4} placeholder={userPageText.textarea.portfolioUrls.placeholder}/>
-                        </div>
-                        <div className="user-page__input-box">
-                            <label htmlFor="projectUrls">{userPageText.textarea.projectUrls.label}</label>
-                            <textarea id="projectUrls" rows={4} placeholder={userPageText.textarea.projectUrls.placeholder}/>
-                        </div>
+                        <Textarea classType={"user-page"} label={userPageText.textarea.bio.label} name={"bio"} rows={4} placeholder={userPageText.textarea.bio.placeholder}/>
+                        <Textarea classType={"user-page"} label={userPageText.textarea.portfolioUrls.label} name={"portfolioUrls"} rows={4} placeholder={userPageText.textarea.portfolioUrls.placeholder}/>
+                        <Textarea classType={"user-page"} label={userPageText.textarea.projectUrls.label} name={"projectUrls"} rows={4} placeholder={userPageText.textarea.projectUrls.placeholder}/>
                     </div>
                     <div className="user-page__col4">
                         <h2>{userPageText.columnTitle.experience}</h2>
-                        <div className="user-page__input-box">
-                            <label htmlFor="education">{userPageText.textarea.education.label}</label>
-                            <textarea id="education" rows={4} placeholder={userPageText.textarea.education.placeholder}/>
-                        </div>
-                        <div className="user-page__input-box">
-                            <label htmlFor="workExperience">{userPageText.textarea.workExperience.label}</label>
-                            <textarea id="workExperience" rows={4} placeholder={userPageText.textarea.workExperience.placeholder}/>
-                        </div>
-                        <div className="user-page__input-box">
-                            <label htmlFor="courses">{userPageText.textarea.courses.label}</label>
-                            <textarea id="courses" rows={4}  placeholder={userPageText.textarea.courses.placeholder}/>
-                        </div>
+                        <Textarea classType={"user-page"} label={userPageText.textarea.education.label} name={"education"} rows={4} placeholder={userPageText.textarea.education.placeholder}/>
+                        <Textarea classType={"user-page"} label={userPageText.textarea.workExperience.label} name={"workExperience"} rows={4} placeholder={userPageText.textarea.workExperience.placeholder}/>
+                        <Textarea classType={"user-page"} label={userPageText.textarea.courses.label} name={"courses"} rows={4} placeholder={userPageText.textarea.courses.placeholder}/>
                     </div>
-
                 </div>
                 <div className="user-page__submit-box">
-                    <button className="btn user-submit-btn">{userPageText.submitButton.text}</button>
+                    <SubmitBtn text={userPageText.submitButton.text} classType={"user-submit-btn"}/>
                 </div>
 
             </Form>
