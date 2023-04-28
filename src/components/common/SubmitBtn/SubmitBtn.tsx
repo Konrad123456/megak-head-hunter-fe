89ac102277eb4a1b6809810c0f10e6957783bf24
Alendart <111238215+Alendart@users.noreply.github.com>
2023-04-28 23:25:33 +0200

    Added option classType for SubmitBtn.tsx

--- src/components/common/SubmitBtn/SubmitBtn.tsx
+++ src/components/common/SubmitBtn/SubmitBtn.tsx
@@ -2,10 +2,11 @@ import "./_SubmitBtn.scss";
 
 type Props = {
   text: string;
+  classType?:string;
 };
-export const SubmitBtn = ({ text }: Props) => {
+export const SubmitBtn = ({ text,classType }: Props) => {
   return (
-    <button type="submit" className="submit-button">
+    <button type="submit" className={`submit-button ${classType ? classType : null}`}>
       {text}
     </button>
   );
