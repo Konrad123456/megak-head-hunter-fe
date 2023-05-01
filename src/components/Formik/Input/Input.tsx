216c8bca1aee318f413f6c13eb3e975f605cdf28
Dariusz Okoński <darek200180@gmail.com>
2023-05-01 16:00:46 +0200

    update input styles

--- src/components/Formik/Input/Input.tsx
+++ src/components/Formik/Input/Input.tsx
@@ -1,28 +1,30 @@
-import {useField} from "formik";
-import React from "react";
-import '../Input/input.scss'
-
+import { useField } from 'formik';
+import React from 'react';
+import '../Input/Input.scss';
 
 interface Props {
-    label:string;
-    name:string;
-    type:string;
-    placeholder:string;
+  label: string;
+  name: string;
+  type: string;
+  placeholder: string;
 }
-export const Input = ({name, type, label,placeholder}: Props) => {
-    const [field, meta] = useField({
-        name: name,
-        type: type,
-    });
-    return(
-        <div className="input-box">
-            <label htmlFor={name}>{label}</label>
-            <input className={name} id={name} {...field} {...{name, type, label,placeholder}}/>
-            {
-                meta.touched && meta.error ?
-                    <div className="error">{meta.error}</div>
-                    : null
-            }
-        </div>
-    )
-}
+export const Input = ({ name, type, label, placeholder }: Props) => {
+  const [field, meta] = useField({
+    name: name,
+    type: type,
+  });
+  return (
+    <div className='input-box'>
+      <label htmlFor={name}>{label}</label>
+      <input
+        className={name}
+        id={name}
+        {...field}
+        {...{ name, type, label, placeholder }}
+      />
+      {meta.touched && meta.error ? (
+        <div className='error'>{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
