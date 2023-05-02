2c9c1c8635ac83839bc8e0dd5652d83c43ddec26
JarekKrolik <madmax@onet.eu>
2023-05-02 18:06:37 +0200

    added builder for sending HR register data

--- src/api/hrRegisterApiSlice.ts
+++ src/api/hrRegisterApiSlice.ts
@@ -1,2 +1,22 @@
 import { apiSlice} from "./apiSlice";
 import { User, withToken } from "../store/auth/types";
+
+interface HrRegisterData {
+    fullName:string;
+    company:string;
+    email:string;
+    maxReservedStudents:number;
+
+}
+
+export const hrRegisterApiSlice = apiSlice.injectEndpoints({
+    endpoints:(builder)=>({
+        sendHr:builder.mutation({
+            query:(hrRegisterData:HrRegisterData)=>({
+                url:'/add_hr',
+                method:'POST',
+                body:hrRegisterData,
+            })
+        })
+    })
+})
