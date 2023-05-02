6e2d63ff23071f885ebccf2294d2453d1df1dd22
Dariusz Okoński <darek200180@gmail.com>
2023-05-02 13:47:18 +0200

    add HumanResourcesSearch

--- src/components/common/Button/SubmitButton.tsx
+++ src/components/common/Button/SubmitButton.tsx
@@ -3,7 +3,10 @@ import './_button.scss';
 type Props = {
   text: string;
   type?: string;
+  handleClick?: () => void;
 };
-export const SubmitButton = ({ text, type = 'submit' }: Props) => {
-  return <input type={type} className='button' value={text} />;
+export const SubmitButton = ({ text, type = 'submit', handleClick }: Props) => {
+  return (
+    <input onClick={handleClick} type={type} className='button' value={text} />
+  );
 };
--- src/pages/HumanResourcesPage/HumanResourcesPage.tsx
+++ src/pages/HumanResourcesPage/HumanResourcesPage.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { HumanResourcesHeader } from '../../components/HumanResources/HumanResourcesHeader/HumanResourcesHeader';
+import { HumanResourcesSearch } from '../../components/HumanResources/HumanResourcesSearch/HumanResourcesSearch';
 
 export const HumanResourcesPage = () => {
   return (
     <div className='human-resources-page'>
       <HumanResourcesHeader />
+
+      <div className='human-resources-page__container'>
+        <HumanResourcesSearch />
+      </div>
     </div>
   );
 };
