a5cbf5b9337a6f8b46b9aa15932ce10643d30c6f
Dariusz Okoński <darek200180@gmail.com>
2023-05-07 14:34:44 +0200

    add paggination

--- src/components/Paggination/Paggination.tsx
+++ src/components/Paggination/Paggination.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
-
 const Paggination = () => {
   return (
     <div className='paggination'>
-      <h1>Paggination to Build</h1>
+      <span className='paggination__elements-number'>Ilość elementów</span>
+      <input
+        className='paggination__input'
+        min={1}
+        max={20}
+        value={10}
+        type='number'
+      />
+      <span className='paggination__numbers'>10 z 11</span>
+      <button
+        disabled={true}
+        className='paggination__button paggination__button--rm'
+      >
+        <span className='paggination__button-span material-symbols-outlined'>
+          arrow_back_ios
+        </span>
+      </button>
+      <button disabled={false} className='paggination__button'>
+        <span className='paggination__button-span material-symbols-outlined'>
+          arrow_forward_ios
+        </span>
+      </button>
     </div>
   );
 };
