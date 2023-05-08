028ae30277da4f610a87632b78ee4176412e6e5b
JarekKrolik <madmax@onet.eu>
2023-05-08 21:58:03 +0200

    added useToTalkMutation file with endpoint

--- src/api/toTalkApiSlice.ts
+++ src/api/toTalkApiSlice.ts
@@ -1,1 +1,15 @@
 import {apiSlice} from "./apiSlice";
+import{StudentsToTalkList}from 'types'
+
+const toTalkApiSlice = apiSlice.injectEndpoints({
+    endpoints:(builder)=>({
+toTalk:builder.mutation<{studentsToTalkList:StudentsToTalkList},{limit:number,page:number}>({
+    query:({limit,page})=>({
+        url:`/user/talk/:${page}/:${limit}`,
+        method:'GET',
+    })
+})
+    })
+})
+
+export const {useToTalkMutation} = toTalkApiSlice;
