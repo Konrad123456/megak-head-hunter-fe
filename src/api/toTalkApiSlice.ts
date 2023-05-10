ebe5490edb06612ea4ad27d249df787760bc9e18
JarekKrolik <madmax@onet.eu>
2023-05-10 16:05:15 +0200

    fixed code formatting in toTalkApiSlice.ts

--- src/api/toTalkApiSlice.ts
+++ src/api/toTalkApiSlice.ts
@@ -1,20 +1,19 @@
 import {apiSlice} from "./apiSlice";
-import{StudentsToTalkList}from 'types'
 
 
 const toTalkApiSlice = apiSlice.injectEndpoints({
-    endpoints:(builder)=>({
-toTalk:builder.mutation<{studentsToTalkList:any},{page:number,limit:number}>({
-    query:({page,limit})=>({
-        url:`/user/talk/${page}/${limit}`,
-        method:'GET',
-    })
-}),
-        removeFromToTalk:builder.mutation({
-            query:(id:string)=>({
-                url:'/user/talk',
-                method:'DELETE',
-                body:{
+    endpoints: (builder) => ({
+        toTalk: builder.mutation<{ studentsToTalkList: any }, { page: number, limit: number }>({
+            query: ({page, limit}) => ({
+                url: `/user/talk/${page}/${limit}`,
+                method: 'GET',
+            })
+        }),
+        removeFromToTalk: builder.mutation({
+            query: (id: string) => ({
+                url: '/user/talk',
+                method: 'DELETE',
+                body: {
                     id,
                 }
             })
@@ -22,4 +21,4 @@ toTalk:builder.mutation<{studentsToTalkList:any},{page:number,limit:number}>({
     })
 })
 
-export const {useToTalkMutation,useRemoveFromToTalkMutation} = toTalkApiSlice;
+export const {useToTalkMutation, useRemoveFromToTalkMutation} = toTalkApiSlice;
