218f7eca731bf2f2913e907a94bc023c3bb61d80
JarekKrolik <madmax@onet.eu>
2023-05-11 06:38:57 +0200

    added downloading registered data for student

--- src/api/getOneStudentApi.ts
+++ src/api/getOneStudentApi.ts
@@ -1,1 +1,14 @@
 import {apiSlice} from "./apiSlice";
+
+const getOneStudentApi = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getStudent: builder.mutation({
+            query: (id:string) => ({
+                url: '/user',
+                method: 'GET',
+            })
+        })
+    })
+})
+
+export const {useGetStudentMutation} = getOneStudentApi
--- src/pages/UserPage/UserPage.tsx
+++ src/pages/UserPage/UserPage.tsx
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from "react";
+import React, {useEffect, useState} from "react";
 
 import "./_user_page.scss"
 import {Logo} from "../../components/Logo/Logo";
@@ -9,74 +9,101 @@ import {useLogoutMutation} from "../../api/authApiSlice";
 import {useDispatch, useSelector} from "react-redux";
 import {logOut, selectCurrentUser} from "../../store/auth/authSlice";
 import {useNavigate} from "react-router";
+import {useGetStudentMutation} from "../../api/getOneStudentApi";
 
-const userMockupData:OneStudentResponse = {
-    firstName: "Katarzyna",
-    lastName: "Testowana",
-    email: "xyz@testowe.pl",
-    tel: "5572813",
-    githubUsername: "ktoś",
-    portfolioUrls: ["pierwszy link","drugi link"],
-    projectUrls: ["pierwszy link","drugi link","trzeci link","czwarty link"],
-    bio: "sialsadlasldaslasldals",
-    expectedContractType: ContractType.B2B_POSSIBLE,
-    // @ts-ignore
-    expectedTypeWork: expectedTypeWorkEntity.ONLY_REMOTELY,
-    targetWorkCity: "Rzeszów",
-    expectedSalary: 12000,
-    canTakeApprenticeship: choiceYesNO.YES,
-    monthsOfCommercialExp: 5,
-    education: "Bardzo długa edukacja",
-    workExperience: "brak doświadczenia",
-    courses: "megaK ukończone z wyróżnieniem",
-}
+// const userMockupData: OneStudentResponse = {
+//     firstName: "Katarzyna",
+//     lastName: "Testowana",
+//     email: "xyz@testowe.pl",
+//     tel: "5572813",
+//     githubUsername: "ktoś",
+//     portfolioUrls: ["pierwszy link", "drugi link"],
+//     projectUrls: ["pierwszy link", "drugi link", "trzeci link", "czwarty link"],
+//     bio: "sialsadlasldaslasldals",
+//     expectedContractType: ContractType.B2B_POSSIBLE,
+//     // @ts-ignore
+//     expectedTypeWork: expectedTypeWorkEntity.ONLY_REMOTELY,
+//     targetWorkCity: "Rzeszów",
+//     expectedSalary: 12000,
+//     canTakeApprenticeship: choiceYesNO.YES,
+//     monthsOfCommercialExp: 5,
+//     education: "Bardzo długa edukacja",
+//     workExperience: "brak doświadczenia",
+//     courses: "megaK ukończone z wyróżnieniem",
+// }
 
 export const UserPage = () => {
-    const [loading,setLoading] = useState<boolean>(true);
+    const [loading, setLoading] = useState<boolean>(true);
     const [logout] = useLogoutMutation();
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const user = useSelector(selectCurrentUser);
-    console.log(user)
+    const [getStudent, {isLoading}] = useGetStudentMutation();
     const onLogOutHandler = async () => {
-      try {
-        await logout({}).unwrap();
-        dispatch(logOut());
-        navigate('/');
-      } catch (e) {
-        console.log(e);
-      }
+        try {
+            await logout({}).unwrap();
+            dispatch(logOut());
+            navigate('/');
+        } catch (e) {
+            console.log(e);
+        }
     }
 
 
     const [data, setData] = useState<OneStudentResponse>({
-        firstName:'',
-        lastName:'',
-        email:'',
-        tel:'',
-        githubUsername:'',
-        portfolioUrls:[''],
-        projectUrls:[''],
-        bio:'',
+        firstName: '',
+        lastName: '',
+        email: '',
+        tel: '',
+        githubUsername: '',
+        portfolioUrls: [''],
+        projectUrls: [''],
+        bio: '',
         expectedContractType: ContractType.NO_PREFERENCE,
         // @ts-ignore
         expectedTypeWork: expectedTypeWorkEntity.IRRELEVANT,
-        targetWorkCity:'',
+        targetWorkCity: '',
         expectedSalary: 0,
         canTakeApprenticeship: choiceYesNO.NO,
         monthsOfCommercialExp: 0,
         education: '',
         workExperience: '',
-        courses:'',
+        courses: '',
     });
     // Here will be fetch for collecting data from BE. Then it will be passed to UserViewForm.
-    useEffect(()=>{
+    useEffect(() => {
         setLoading(true);
-        setData(userMockupData);
-        setLoading(false);
-    },[])
+        (async ()=>{
+            const response = await getStudent('id')
+            // @ts-ignore
+            const data = response.data as OneStudentResponse
+            const userData = {
+                firstName: data.firstName,
+                lastName: data.lastName,
+                email: data.email,
+                tel: data.tel,
+                githubUsername: data.githubUsername,
+                portfolioUrls: data.portfolioUrls,
+                projectUrls: data.projectUrls,
+                bio: data.bio,
+                expectedContractType: data.expectedContractType,
+                // @ts-ignore
+                expectedTypeWork: data.expectedTypeWorkEntity,
+                targetWorkCity: data.targetWorkCity,
+                expectedSalary: data.expectedSalary,
+                canTakeApprenticeship: data.canTakeApprenticeship,
+                monthsOfCommercialExp: data.monthsOfCommercialExp,
+                education: data.education,
+                workExperience: data.workExperience,
+                courses: data.courses,
+            }
+            setData(userData);
+            setLoading(false);
+        })()
 
-    return(
+
+    }, [])
+
+    return (
         <>
             <div className='user-page'>
                 <div className='user-page__header'>
