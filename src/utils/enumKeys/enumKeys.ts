64f8811872cf73a650aec35d4220b29d464dba66
Rafal Nalewajek <rafal.nalewajek@gmail.com>
2023-05-17 17:32:44 +0200

    added new functions.

--- src/utils/enumKeys/enumKeys.ts
+++ src/utils/enumKeys/enumKeys.ts
@@ -1,4 +1,7 @@
 import { ContractType, expectedTypeWorkEntity } from "types"
 
-export const expectedContractTypesValues = Object.keys(ContractType).filter(e => e.length > 1 )
-export const expectedTypeWorkValues = Object.keys(expectedTypeWorkEntity).filter(e => e.length > 1 )
+export const expectedContractTypesValues = Object.keys(ContractType).filter(e => isNaN(Number(e)));
+export const expectedContractTypesValuesNum = Object.keys(ContractType).filter(e => !isNaN(Number(e)));
+
+export const expectedTypeWorkValues = Object.keys(expectedTypeWorkEntity).filter(e => isNaN(Number(e)));
+export const expectedTypeWorkValuesNum = Object.keys(expectedTypeWorkEntity).filter(e => !isNaN(Number(e)));
